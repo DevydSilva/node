f75bbb7539911f6efec78fb475e15e7afacf8d34
Devyd Silva <devyd.santana20201@gmail.com>
2024-11-01 14:31:31 -0300

    novo

--- src/shared/typeorm/migrations/1727887197770-CreateProducts.ts
+++ src/shared/typeorm/migrations/1727887197770-CreateProducts.ts
@@ -9,12 +9,53 @@ export class CreateProducts1727887197770 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quatity',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
           }
         ]
       }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.dropTable('products');
     }
 
 }
+
+
+/*{
+  name: 'created_at',
+  type: 'timestamp with time zone',
+  default: 'now()',
+},
+{
+  name: 'updated_at',
+  type: 'timestamp with time zone',
+  default: 'now()',
+},
+ */
